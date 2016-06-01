314104f0064535326a5b25a24fbd3cfce744b474
Jun Wu <quark@lihdd.net>
2016-06-01 22:18:14 +0100

    version: include git commit hash
    Instead of an ambiguous "git", include git commit hash in the VERSION string.

--- lib/sup/version.rb
+++ lib/sup/version.rb
@@ -1,3 +1,12 @@
 module Redwood
-  VERSION = "git"
+  def self.get_version
+    git_dir = File.expand_path(File.join(File.dirname(__FILE__), '../../.git'))
+    if Dir.exist?(git_dir)
+      'g' + IO::popen(['git', '--git-dir', git_dir, 'rev-parse', 'HEAD']).read[0, 7]
+    else
+      'unknown'
+    end rescue 'unknown'
+  end
+
+  VERSION = get_version
 end
